f2a8677eec30f0dd06736c389f902d5e7c596623
Nguyen Duc Duy <39079556+duyelbi@users.noreply.github.com>
2021-08-16 02:24:55 +0700

    add material ui theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 import {
-    Container
-} from '@material-ui/core';
+  Container,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline,
+} from "@material-ui/core";
 
-import Header from './Header';
-import Footer from './Footer';
+import Header from "./Header";
+import Footer from "./Footer";
 
-import useStyles from '../utils/styles';
+import useStyles from "../utils/styles";
 
 const Layout = ({ title, description, children }) => {
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      body1: {
+        fontWeight: "normal",
+      },
+    },
+    palette: {
+      type: "light",
+      primary: {
+        main: "#121212",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
   const classes = useStyles();
   return (
     <div>
@@ -17,9 +47,12 @@ const Layout = ({ title, description, children }) => {
         <title>{title ? `${title} - V-Market` : `V-Market`}</title>
         <meta name="description" content={description}></meta>
       </Head>
-      <Header />
-      <Container className={classes.main}>{children}</Container>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Container className={classes.main}>{children}</Container>
+        <Footer />
+      </ThemeProvider>
     </div>
   );
 };
