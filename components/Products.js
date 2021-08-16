2b5f64072de20c7dfe6e4ae988960ab109379820
Nguyen Duc Duy <39079556+duyelbi@users.noreply.github.com>
2021-08-16 10:01:07 +0700

    fetch products from api

--- components/Products.js
+++ components/Products.js
@@ -11,40 +11,53 @@ import {
   Typography,
 } from "@material-ui/core";
 
-import data from "../utils/data";
+import db from "../utils/db";
+import Product from "../models/Product";
 
-const Products = () => {
-    return (
-      <div>
-          <h1>Products</h1>
-          <Grid container spacing={3}>
-            {data.products.map((products) => (
-              <Grid item md={3} key={products.name}>
-                <Card>
-                  <NextLink href={`/products/${products.slug}`} passHref>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      image={products.image}
-                      title={products.name}
-                    ></CardMedia>
-                    <CardContent>
-                      <Typography>{products.name}</Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  </NextLink>
-                  <CardActions>
-                    <Typography>${products.price}</Typography>
-                    <Button size="small" color="primary">
-                      Add to cart
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+const Products = (props) => {
+  const { products } = props;
+  return (
+    <div>
+      <h1>Products</h1>
+      <Grid container spacing={3}>
+        {products.map((product) => (
+          <Grid item md={3} key={product.name}>
+            <Card>
+              <NextLink href={`/products/${product.slug}`} passHref>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </NextLink>
+              <CardActions>
+                <Typography>${product.price}</Typography>
+                <Button size="small" color="primary">
+                  Add to cart
+                </Button>
+              </CardActions>
+            </Card>
           </Grid>
-      </div>
-    );
-}
+        ))}
+      </Grid>
+    </div>
+  );
+};
 
 export default Products;
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
+}
